a013299affbd92fcf62cce1a22ef4f30b0e23333
Gh0stbasta <ubuntuvj@gmail.com>
2025-01-28 21:02:15 +0100

    practicing routes in express.js

--- 02 - In Progress/expressJsonServer/exServer.js
+++ 02 - In Progress/expressJsonServer/exServer.js
@@ -15,16 +15,226 @@ import {
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
+app.delete("/countrys/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = countryData.findIndex((country) => country.id === id);
+  try {
+    const delCountry = countryData.splice(index, 1);
+    res.send({ message: "country deleted", delCountry });
+  } catch (error) {
+    res.status(404).send({ error: "country not found " });
+  }
+});
 
+app.delete("/islands/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = islandData.findIndex((island) => island.id === id);
+  try {
+    const delIsland = islandData.splice(index, 1);
+    res.send({ message: "island deleted", delIsland });
+  } catch (error) {
+    res.status(404).send({ error: "island not found" });
+  }
+});
+
+app.delete("/citys/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = cityData.findIndex((city) => city.id === id);
+  try {
+    const delCity = cityData.splice(index, 1);
+    res.send({ message: "city deleted", delCity });
+  } catch (error) {
+    res.status(404).send({ error: "city not found" });
+  }
+});
+
+app.delete("/rivers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = riverData.findIndex((river) => river.id === id);
+  try {
+    const delRiver = riverData.splice(index, 1);
+    res.send({ message: "river deleted", delRiver });
+  } catch (error) {
+    res.status(404).send({ error: "river not found" });
+  }
+});
+
+app.delete("/mountains/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = mountainData.findIndex((mountain) => mountain.id === id);
+  try {
+    const delMountain = mountainData.splice(index, 1);
+    res.send({ message: "Mountain deleted", delMountain });
+  } catch (error) {
+    res.status(404).send({ error: "Did not find mountain" });
+  }
+});
+
+app.delete("/lakes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = lakeData.findIndex((lake) => lake.id === id);
+  try {
+    const delLake = lakeData.splice(index, 1);
+    res.send({ message: "Lake deleted", delLake });
+  } catch (error) {
+    res.status(404).send({ error: "lake not found" });
+  }
+});
+
+app.delete("/deserts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = desertData.findIndex((desert) => desert.id === id);
+  try {
+    const delDesert = desertData.splice(index, 1);
+    res.send({ message: "Desert deleted", delDesert });
+  } catch (error) {
+    res.status(404).send({ error: "Desert not found" });
+  }
+});
+
+app.delete("/oceans/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = oceanData.findIndex((ocean) => ocean.id === id);
+  try {
+    const delOcean = oceanData.splice(index, 1);
+    res.send({ message: "Ocean deleted", delOcean });
+  } catch (error) {
+    res.status(404).send({ error: "Ocean not found" });
+  }
+});
+
+app.delete("/volcanos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = volcanoData.findIndex((forest) => forest.id === id);
+  try {
+    const delVolcano = volcanoData.splice(index, 1);
+    res.send({ message: "Volcano deleted", delVolcano });
+  } catch (error) {
+    res.status(404).send({ error: "Volcano not found" });
+  }
+});
+
+app.delete("/forests/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = forestData.findIndex((forest) => forest.id === id);
+  try {
+    const delForest = forestData.splice(index, 1);
+    res.send({ message: "forest deleted", delForest });
+  } catch (error) {
+    res.status(404).send({ error: "Forest not found" });
+  }
+});
+
+app.delete("/waterfalls/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = waterfallData.findIndex((waterfall) => waterfall.id === id);
+  try {
+    const deletedWaterfall = waterfallData.splice(index, 1);
+    res.send({ message: "Waterfall deleted successfully", deletedWaterfall });
+  } catch (error) {
+    res.status(404).send({ error: "Waterfall not found" });
+  }
+});
+
+app.get("/waterfalls", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * waterfallData.length);
+    res.send(waterfallData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching waterfallData" });
+  }
+});
+
+app.get("/forests", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * forestData.length);
+    res.send(forestData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching forestData" });
+  }
+});
+
+app.get("/volcanos", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * volcanoData.length);
+    res.send(volcanoData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching volcanoData" });
+  }
+});
+
+app.get("/oceans", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * oceanData.length);
+    res.send(oceanData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching oceanData" });
+  }
+});
+
+app.get("/deserts", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * desertData.length);
+    res.send(desertData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching desertData" });
+  }
+});
+
+app.get("/lakes", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * lakeData.length);
+    res.send(lakeData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching lakeData" });
+  }
+});
+
+app.get("/mountains", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * mountainData.length);
+    res.send(mountainData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching mountainData" });
+  }
+});
+
+app.get("/rivers", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * riverData.length);
+    res.send(riverData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Fetching riverData failed" });
+  }
+});
+
+app.get("/citys", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * cityData.length);
+    res.send(cityData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Fetching cityData failed" });
+  }
+});
+
+app.get("/islands", (req, res) => {
+  try {
+    const randomIndex = Math.floor(Math.random() * islandData.length);
+    res.send(islandData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Fetching failed on islandData" });
+  }
+});
 
 app.get("/countrys", (req, res) => {
-    try {
-        const randomIndex = Math.floor(Math.random() * countryData.length);
-        res.send(countryData[randomIndex]);
-    } catch (error) {
-        res.status(500).send({ error: "An error occurred while fetching country data." });
-    }
+  try {
+    const randomIndex = Math.floor(Math.random() * countryData.length);
+    res.send(countryData[randomIndex]);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching Data" });
+  }
 });
 
 app.listen(port, () => {
--- 02 - In Progress/expressJsonServer/jsonData.js
+++ 02 - In Progress/expressJsonServer/jsonData.js
@@ -1,50 +1,60 @@
 export let countryData = [
   {
+    id: 1,
     Country: "Germany",
     Capital: "Berlin",
     Population: "83 million",
   },
   {
+    id: 2,
     Country: "France",
     Capital: "Paris",
     Population: "67 million",
   },
   {
+    id: 3,
     Country: "Italy",
     Capital: "Rome",
     Population: "60 million",
   },
   {
+    id: 4,
     Country: "Spain",
     Capital: "Madrid",
     Population: "47 million",
   },
   {
+    id: 5,
     Country: "United Kingdom",
     Capital: "London",
     Population: "66 million",
   },
   {
+    id: 6,
     Country: "Canada",
     Capital: "Ottawa",
     Population: "38 million",
   },
   {
+    id: 7,
     Country: "Australia",
     Capital: "Canberra",
     Population: "25 million",
   },
   {
+    id: 8,
     Country: "Japan",
     Capital: "Tokyo",
     Population: "126 million",
   },
   {
+    id: 9,
     Country: "Brazil",
     Capital: "Brasilia",
     Population: "211 million",
   },
   {
+    id: 10,
     Country: "India",
     Capital: "New Delhi",
     Population: "1.3 billion",
@@ -53,51 +63,61 @@ export let countryData = [
 
 export let cityData = [
   {
+    id: 1,
     City: "New York",
     Country: "United States",
     Population: "8.4 million",
   },
   {
+    id: 2,
     City: "Los Angeles",
     Country: "United States",
     Population: "4 million",
   },
   {
+    id: 3,
     City: "Chicago",
     Country: "United States",
     Population: "2.7 million",
   },
   {
+    id: 4,
     City: "Houston",
     Country: "United States",
     Population: "2.3 million",
   },
   {
+    id: 5,
     City: "Phoenix",
     Country: "United States",
     Population: "1.7 million",
   },
   {
+    id: 6,
     City: "Philadelphia",
     Country: "United States",
     Population: "1.6 million",
   },
   {
+    id: 7,
     City: "San Antonio",
     Country: "United States",
     Population: "1.5 million",
   },
   {
+    id: 8,
     City: "San Diego",
     Country: "United States",
     Population: "1.4 million",
   },
   {
+    id: 9,
     City: "Dallas",
     Country: "United States",
     Population: "1.3 million",
   },
   {
+    id: 10,
     City: "San Jose",
     Country: "United States",
     Population: "1 million",
@@ -106,51 +126,61 @@ export let cityData = [
 
 export let riverData = [
   {
+    id: 1,
     River: "Nile",
     Length: "6,650 km",
     Continent: "Africa",
   },
   {
+    id: 2,
     River: "Amazon",
     Length: "6,400 km",
     Continent: "South America",
   },
   {
+    id: 3,
     River: "Yangtze",
     Length: "6,300 km",
     Continent: "Asia",
   },
   {
+    id: 4,
     River: "Mississippi",
     Length: "6,275 km",
     Continent: "North America",
   },
   {
+    id: 5,
     River: "Yenisei",
     Length: "5,539 km",
     Continent: "Asia",
   },
   {
+    id: 6,
     River: "Yellow",
     Length: "5,464 km",
     Continent: "Asia",
   },
   {
+    id: 7,
     River: "Ob",
     Length: "5,410 km",
     Continent: "Asia",
   },
   {
+    id: 8,
     River: "Parana",
     Length: "4,880 km",
     Continent: "South America",
   },
   {
+    id: 9,
     River: "Congo",
     Length: "4,700 km",
     Continent: "Africa",
   },
   {
+    id: 10,
     River: "Amur",
     Length: "4,444 km",
     Continent: "Asia",
@@ -159,51 +189,61 @@ export let riverData = [
 
 export let mountainData = [
   {
+    id: 1,
     Mountain: "Mount Everest",
     Height: "8,848 m",
     Range: "Himalayas",
   },
   {
+    id: 2,
     Mountain: "K2",
     Height: "8,611 m",
     Range: "Karakoram",
   },
   {
+    id: 3,
     Mountain: "Kangchenjunga",
     Height: "8,586 m",
     Range: "Himalayas",
   },
   {
+    id: 4,
     Mountain: "Lhotse",
     Height: "8,516 m",
     Range: "Himalayas",
   },
   {
+    id: 5,
     Mountain: "Makalu",
     Height: "8,485 m",
     Range: "Himalayas",
   },
   {
+    id: 6,
     Mountain: "Cho Oyu",
     Height: "8,188 m",
     Range: "Himalayas",
   },
   {
+    id: 7,
     Mountain: "Dhaulagiri",
     Height: "8,167 m",
     Range: "Himalayas",
   },
   {
+    id: 8,
     Mountain: "Manaslu",
     Height: "8,163 m",
     Range: "Himalayas",
   },
   {
+    id: 9,
     Mountain: "Nanga Parbat",
     Height: "8,126 m",
     Range: "Himalayas",
   },
   {
+    id: 10,
     Mountain: "Annapurna",
     Height: "8,091 m",
     Range: "Himalayas",
@@ -212,51 +252,61 @@ export let mountainData = [
 
 export let lakeData = [
   {
+    id: 1,
     Lake: "Caspian Sea",
     Area: "371,000 km²",
     Continent: "Asia",
   },
   {
+    id: 2,
     Lake: "Superior",
     Area: "82,100 km²",
     Continent: "North America",
   },
   {
+    id: 3,
     Lake: "Victoria",
     Area: "68,800 km²",
     Continent: "Africa",
   },
   {
+    id: 4,
     Lake: "Huron",
     Area: "59,600 km²",
     Continent: "North America",
   },
   {
+    id: 5,
     Lake: "Michigan",
     Area: "58,000 km²",
     Continent: "North America",
   },
   {
+    id: 6,
     Lake: "Tanganyika",
     Area: "32,900 km²",
     Continent: "Africa",
   },
   {
+    id: 7,
     Lake: "Baikal",
     Area: "31,500 km²",
     Continent: "Asia",
   },
   {
+    id: 8,
     Lake: "Great Bear Lake",
     Area: "31,000 km²",
     Continent: "North America",
   },
   {
+    id: 9,
     Lake: "Malawi",
     Area: "29,500 km²",
     Continent: "Africa",
   },
   {
+    id: 10,
     Lake: "Great Slave Lake",
     Area: "28,600 km²",
     Continent: "North America",
@@ -265,51 +315,61 @@ export let lakeData = [
 
 export let desertData = [
   {
+    id: 1,
     Desert: "Sahara",
     Area: "9,200,000 km²",
     Continent: "Africa",
   },
   {
+    id: 2,
     Desert: "Arabian",
     Area: "2,330,000 km²",
     Continent: "Asia",
   },
   {
+    id: 3,
     Desert: "Gobi",
     Area: "1,295,000 km²",
     Continent: "Asia",
   },
   {
+    id: 4,
     Desert: "Kalahari",
     Area: "900,000 km²",
     Continent: "Africa",
   },
   {
+    id: 5,
     Desert: "Patagonian",
     Area: "673,000 km²",
     Continent: "South America",
   },
   {
+    id: 6,
     Desert: "Great Victoria",
     Area: "647,000 km²",
     Continent: "Australia",
   },
   {
+    id: 7,
     Desert: "Syrian",
     Area: "520,000 km²",
     Continent: "Asia",
   },
   {
+    id: 8,
     Desert: "Great Basin",
     Area: "492,000 km²",
     Continent: "North America",
   },
   {
+    id: 9,
     Desert: "Chihuahuan",
     Area: "450,000 km²",
     Continent: "North America",
   },
   {
+    id: 10,
     Desert: "Karakum",
     Area: "350,000 km²",
     Continent: "Asia",
@@ -318,26 +378,31 @@ export let desertData = [
 
 export let oceanData = [
   {
+    id: 1,
     Ocean: "Pacific",
     Area: "168,723,000 km²",
     Depth: "10,911 m",
   },
   {
+    id: 2,
     Ocean: "Atlantic",
     Area: "85,133,000 km²",
     Depth: "8,486 m",
   },
   {
+    id: 3,
     Ocean: "Indian",
     Area: "70,560,000 km²",
     Depth: "7,906 m",
   },
   {
+    id: 4,
     Ocean: "Southern",
     Area: "21,960,000 km²",
     Depth: "7,236 m",
   },
   {
+    id: 5,
     Ocean: "Arctic",
     Area: "15,558,000 km²",
     Depth: "5,450 m",
@@ -346,51 +411,61 @@ export let oceanData = [
 
 export let islandData = [
   {
+    id: 1,
     Island: "Greenland",
     Area: "2,166,086 km²",
     Country: "Denmark",
   },
   {
+    id: 2,
     Island: "New Guinea",
     Area: "785,753 km²",
     Country: "Indonesia/Papua New Guinea",
   },
   {
+    id: 3,
     Island: "Borneo",
     Area: "748,168 km²",
     Country: "Indonesia/Malaysia/Brunei",
   },
   {
+    id: 4,
     Island: "Madagascar",
     Area: "587,041 km²",
     Country: "Madagascar",
   },
   {
+    id: 5,
     Island: "Baffin",
     Area: "507,451 km²",
     Country: "Canada",
   },
   {
+    id: 6,
     Island: "Sumatra",
     Area: "473,481 km²",
     Country: "Indonesia",
   },
   {
+    id: 7,
     Island: "Honshu",
     Area: "225,800 km²",
     Country: "Japan",
   },
   {
+    id: 8,
     Island: "Victoria",
     Area: "217,291 km²",
     Country: "Canada",
   },
   {
+    id: 9,
     Island: "Great Britain",
     Area: "209,331 km²",
     Country: "United Kingdom",
   },
   {
+    id: 10,
     Island: "Ellesmere",
     Area: "196,236 km²",
     Country: "Canada",
@@ -399,51 +474,61 @@ export let islandData = [
 
 export let volcanoData = [
   {
+    id: 1,
     Volcano: "Mauna Loa",
     Height: "4,169 m",
     Location: "Hawaii, USA",
   },
   {
+    id: 2,
     Volcano: "Mount St. Helens",
     Height: "2,550 m",
     Location: "Washington, USA",
   },
   {
+    id: 3,
     Volcano: "Mount Fuji",
     Height: "3,776 m",
     Location: "Honshu, Japan",
   },
   {
+    id: 4,
     Volcano: "Mount Vesuvius",
     Height: "1,281 m",
     Location: "Campania, Italy",
   },
   {
+    id: 5,
     Volcano: "Krakatoa",
     Height: "813 m",
     Location: "Sunda Strait, Indonesia",
   },
   {
+    id: 6,
     Volcano: "Mount Etna",
     Height: "3,329 m",
     Location: "Sicily, Italy",
   },
   {
+    id: 7,
     Volcano: "Mount Kilimanjaro",
     Height: "5,895 m",
     Location: "Tanzania",
   },
   {
+    id: 8,
     Volcano: "Mount Pinatubo",
     Height: "1,486 m",
     Location: "Luzon, Philippines",
   },
   {
+    id: 9,
     Volcano: "Mount St. Helens",
     Height: "2,550 m",
     Location: "Washington, USA",
   },
   {
+    id: 10,
     Volcano: "Mount Rainier",
     Height: "4,392 m",
     Location: "Washington, USA",
@@ -452,51 +537,61 @@ export let volcanoData = [
 
 export let forestData = [
   {
+    id: 1,
     Forest: "Amazon Rainforest",
     Area: "5,500,000 km²",
     Location: "South America",
   },
   {
+    id: 2,
     Forest: "Congo Rainforest",
     Area: "1,780,000 km²",
     Location: "Africa",
   },
   {
+    id: 3,
     Forest: "Valdivian Temperate Rainforest",
     Area: "248,100 km²",
     Location: "South America",
   },
   {
+    id: 4,
     Forest: "Tongass National Forest",
     Area: "68,000 km²",
     Location: "Alaska, USA",
   },
   {
+    id: 5,
     Forest: "Daintree Rainforest",
     Area: "1,200 km²",
     Location: "Australia",
   },
   {
+    id: 6,
     Forest: "Sundarbans",
     Area: "10,000 km²",
     Location: "Bangladesh/India",
   },
   {
+    id: 7,
     Forest: "Black Forest",
     Area: "6,009 km²",
     Location: "Germany",
   },
   {
+    id: 8,
     Forest: "Białowieża Forest",
     Area: "1,500 km²",
     Location: "Poland/Belarus",
   },
   {
+    id: 9,
     Forest: "Sinharaja Forest Reserve",
     Area: "88.64 km²",
     Location: "Sri Lanka",
   },
   {
+    id: 10,
     Forest: "Monteverde Cloud Forest Reserve",
     Area: "105 km²",
     Location: "Costa Rica",
@@ -505,51 +600,61 @@ export let forestData = [
 
 export let waterfallData = [
   {
+    id: 1,
     Waterfall: "Angel Falls",
     Height: "979 m",
     Location: "Venezuela",
   },
   {
+    id: 2,
     Waterfall: "Tugela Falls",
     Height: "948 m",
     Location: "South Africa",
   },
   {
+    id: 3,
     Waterfall: "Cataratas las Tres Hermanas",
     Height: "914 m",
     Location: "Peru",
   },
   {
+    id: 4,
     Waterfall: "Olo'upena Falls",
     Height: "900 m",
     Location: "Hawaii, USA",
   },
   {
+    id: 5,
     Waterfall: "Yumbilla Falls",
     Height: "896 m",
     Location: "Peru",
   },
   {
+    id: 6,
     Waterfall: "Vinnufossen",
     Height: "860 m",
     Location: "Norway",
   },
   {
+    id: 7,
     Waterfall: "Balaifossen",
     Height: "850 m",
     Location: "Norway",
   },
   {
+    id: 8,
     Waterfall: "Pu'uka'oku Falls",
     Height: "840 m",
     Location: "Hawaii, USA",
   },
   {
+    id: 9,
     Waterfall: "James Bruce Falls",
     Height: "840 m",
     Location: "British Columbia, Canada",
   },
   {
+    id: 10,
     Waterfall: "Browne Falls",
     Height: "836 m",
     Location: "New Zealand",
