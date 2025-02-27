130c60f09e0e8fc99f2b6fe063f0c2739d4e4d55
Gh0stbasta <ubuntuvj@gmail.com>
2025-02-27 10:46:37 +0100

    Braintwist

--- 02InProgress/simpleNode/echoBraintwist.js
+++ 02InProgress/simpleNode/echoBraintwist.js
@@ -1,6 +1,6 @@
 const readline = require('readline'); // okay, we need some kind of module to read every line a user gives to us
 
-const rl = readline.createInterface({ // We need to create an interface for readling to tell him where the cool stuff happens
+const rl = readline.createInterface({ // We need to create an interface for readline to tell him where the cool stuff happens
     input: process.stdin, // This is were we define what the interface has to read. It reads keyboard input from the users
     output: process.stdout // this is were we define where we show our stuff. This is just the console
 });
@@ -15,7 +15,7 @@ const answers = [
   // this is actually pretty cool, since this is an array of fucking functions!!!!!! Can you believe it???? FUCKING FUNCTIONS IN AN ARRAY!!!!!
   (name) => `\n\nNice to meet you, ${name.trim()}.`, // only made possible since in JS even Functions are just Objects.
   (age) =>
-    `\n\nOhhh.... ${age.trim()} is really old. Which sport are you doing to keep fit?`, // I hate the template stringt, but this has style!
+    `\n\nOhhh.... ${age.trim()} is really old. Which sport are you doing to keep fit?`, // I hate the template string, but this has style!
   (sport) =>
     `\n\n${sport.trim()} is a nice way of staying fit. Do you like the conversation?`,
   (response) =>
@@ -32,7 +32,7 @@ const askQuestion = () => { // setting up the function to ask some questions
             askQuestion(); // and then FUCKING RECURSE to our questions again to stay in line since we're doing this shit async and this is the only way we make sure, that this gets done one by one
         });
     } else {
-        rl.close(); // yup, when were done were going home :)
+        rl.close(); // yup, when were done were going home :) ... and WE ARE FUCKING DONE!!!!
     }
 };
 
